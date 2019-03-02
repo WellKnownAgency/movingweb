a3f7f6059d7f6cea35eab66ae0854e80d45a45fa
Taras <taraskim@wknown.com>
2019-03-02 13:49:27 -0500

    Addded Colors + Changed Header

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -16,3 +16,4 @@
 @yield('preload')
 <link href="{!! asset('css/app.css') !!}" media="all" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
