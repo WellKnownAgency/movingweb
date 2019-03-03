0ce3468f3d91949365c93d576b667336c4194b53
Taras <taraskim@wknown.com>
2019-03-02 21:56:38 -0500

    New Colors, Added Reviews section

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-color-on-scroll fixed-top navbar-expand-lg navbar-transparent" color-on-scroll="100" id="sectionsNav">
     <div class="container">
       <div class="navbar-translate">
-        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/index.html">
-          Material Kit </a>
+        <a class="navbar-brand" href="/">
+          MovingCRM </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="sr-only">Toggle navigation</span>
           <span class="navbar-toggler-icon"></span>
@@ -13,17 +13,9 @@
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
           <li class="dropdown nav-item">
-            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-              <i class="material-icons">apps</i> Components
+            <a href="/" class="nav-link">
+              <i class="material-icons">attach_money</i> Pricing
             </a>
-            <div class="dropdown-menu dropdown-with-icons">
-              <a href="./index.html" class="dropdown-item">
-                <i class="material-icons">layers</i> All Components
-              </a>
-              <a href="https://demos.creative-tim.com/material-kit/docs/2.1/getting-started/introduction.html" class="dropdown-item">
-                <i class="material-icons">content_paste</i> Documentation
-              </a>
-            </div>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
---
+++ resources/views/sections/reviews.blade.php
@@ -0,0 +1,61 @@
+<div class="section text-center">
+	<h2 class="title">Testimonials</h2>
+	<div class="team">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card card-blog">
+			    <div class="card-header card-header-image">
+			        <a href="#pablo">
+			            <img class="img" src="https://images.unsplash.com/photo-1511439817358-bee8e21790b5?auto=format&fit=crop&w=750&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D">
+			            <div class="card-title">
+			                This Summer Will be Awesome
+			            </div>
+			        </a>
+			    </div>
+			    <div class="card-body">
+			        <h6 class="card-category text-info">Fashion</h6>
+			        <p class="card-description">
+			            Don&apos;t be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens&#x2019; bed design but the back is...
+			        </p>
+			    </div>
+			</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card card-blog">
+				    <div class="card-header card-header-image">
+				        <a href="#pablo">
+				            <img class="img" src="https://images.unsplash.com/photo-1511439817358-bee8e21790b5?auto=format&fit=crop&w=750&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D">
+				            <div class="card-title">
+				                This Summer Will be Awesome
+				            </div>
+				        </a>
+				    </div>
+				    <div class="card-body">
+				        <h6 class="card-category text-info">Fashion</h6>
+				        <p class="card-description">
+				            Don&apos;t be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens&#x2019; bed design but the back is...
+				        </p>
+				    </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card card-blog">
+				    <div class="card-header card-header-image">
+				        <a href="#pablo">
+				            <img class="img rounded" src="https://images.unsplash.com/photo-1511439817358-bee8e21790b5?auto=format&fit=crop&w=750&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D">
+				            <div class="card-title">
+				                This Summer Will be Awesome
+				            </div>
+				        </a>
+				    </div>
+				    <div class="card-body">
+				        <h6 class="card-category text-info">Fashion</h6>
+				        <p class="card-description">
+				            Don&apos;t be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens&#x2019; bed design but the back is...
+				        </p>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
