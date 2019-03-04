9208ad6b75d0d4df966eacf2783422e571df8de4
User1 <user1@User1s-iMac.local>
2019-03-04 16:35:56 -0500

    Icon Section

---
+++ resources/views/sections/icons.blade.php
@@ -0,0 +1,52 @@
+<div class="features">
+  <div class="row">
+    <div class="col-md-2">
+      <div class="info info_custom">
+        <div class="icon icon-info">
+          <i class="material-icons">chat</i>
+        </div>
+        <h4 class="info-title">Free Chat</h4>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="info info_custom">
+        <div class="icon icon-success">
+          <i class="material-icons">verified_user</i>
+        </div>
+        <h4 class="info-title">Verified Users</h4>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="info info_custom">
+        <div class="icon icon-danger">
+          <i class="material-icons">fingerprint</i>
+        </div>
+        <h4 class="info-title">Fingerprint</h4>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="info info_custom">
+        <div class="icon icon-danger">
+          <i class="material-icons">fingerprint</i>
+        </div>
+        <h4 class="info-title">Fingerprint</h4>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="info info_custom">
+        <div class="icon icon-danger">
+          <i class="material-icons">fingerprint</i>
+        </div>
+        <h4 class="info-title">Fingerprint</h4>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="info info_custom">
+        <div class="icon icon-danger">
+          <i class="material-icons">fingerprint</i>
+        </div>
+        <h4 class="info-title">Fingerprint</h4>
+      </div>
+    </div>
+  </div>
+</div>
