32e442355df191167595ba583c52f2a59f65dec5
Taras <taraskim@wknown.com>
2019-05-16 08:43:58 -0400

    fices to testimonials

--- resources/views/sections/reviews.blade.php
+++ resources/views/sections/reviews.blade.php
@@ -3,11 +3,11 @@
 	<br>
 	<div class="team">
 		<div class="row">
-			<div class="col-md-4 offset-md-4">
+			<div class="col-md-4 offset-md-2">
 				<div class="card card-profile">
 			    <div class="card-avatar">
-			        <a href="#pablo">
-			            <img class="img" src="https://images.unsplash.com/photo-1511439817358-bee8e21790b5?auto=format&fit=crop&w=750&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D">
+			        <a href="#">
+			            <img class="img" src="/images/blinkmoving-square.png" width="130px;" height="100%">
 			        </a>
 			    </div>
 			    <div class="card-body">
@@ -20,23 +20,23 @@
 			    </div>
 			</div>
 			</div>
-			<!-- <div class="col-md-4">
+			<div class="col-md-4">
 				<div class="card card-profile">
 			    <div class="card-avatar">
 			        <a href="#pablo">
-			            <img class="img" src="https://images.unsplash.com/photo-1511439817358-bee8e21790b5?auto=format&fit=crop&w=750&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D">
+			            <img class="img" src="/images/moverstomove.png" width="130px" height="100%">
 			        </a>
 			    </div>
 			    <div class="card-body">
-			        <h6 class="card-category text-info">Born to Move</h6>
+			        <h6 class="card-category text-info">Movers to Move</h6>
 							<blockquote class="blockquote">
 				        <p class="card-description">
-				          Easy Moving software solution for your needs...
+				          Great Moving Compny Software for Small Companies. Fair Price and Secured work
 				        </p>
 							</blockquote>
 			    </div>
 			</div>
-		</div> -->
+		</div>
 			<!-- <div class="col-md-4">
 				<div class="card card-profile">
 			    <div class="card-avatar">
