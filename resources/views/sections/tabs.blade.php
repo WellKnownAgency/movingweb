d83e593de475e57d4f2ec329c5ad1f648be6af1f
Taras Kim <kimtaras@gmail.com>
2019-12-17 16:32:52 -0500

    fixes

--- resources/views/sections/tabs.blade.php
+++ resources/views/sections/tabs.blade.php
@@ -1,265 +1,154 @@
-<div class="section justify-content-center">
-	<h2 class="title text-center">What is MovingCRM?</h2>
-	<br>
-	<div id="slideshow">
-		<div class="slide">
-			<div class="caption">
-				<h2><i class="fas fa-calculator"></i> Moving Cost Calculator</h2>
-				<p>
-					A simple 2 steps Calculator for your clients to get a Moving Etimate<br>
-				And for you to get an information about the lead.
-				</p>
-			</div>
-			<img src="/images/calculator.png" />
-		</div>
-		<div class="slide">
-			<div class="caption">
-					<h2><i class="far fa-id-card"></i> Moving Requests</h2>
-					<p>
-						Manage your Request in a simple way. Change Moving Dates, Travel Time and more.
-					</p>
-				</div>
-			<img src="/images/requests.png" />
-		</div>
-		<div class="slide">
-		<div class="caption">
-					<h2><i class="far fa-calendar-alt"></i> Moving Schedule</h2>
-					<p>
-					Create Moving Schedule and manage all works through <b>Moving Calendar</b><br>
-						Everything you need in a simple design.
-					</p>
-				</div>
-			<img src="/images/schedule.png" />
-		</div>
-		<div class="slide">
-			<div class="caption">
-				<h2><i class="fas fa-users"></i> Crew Management</h2>
-				<p>
-				Manage yout teams and assign them to works<br>
-						Provide information to your workers about your customer.
-				</p>
-			</div>
-			<img src="/images/team.png" />
-		</div>
-		<div class="slide">
-			<div class="caption">
-				<h2><i class="fas fa-mail-bulk"></i> Marketing Autmation</h2>
-				<p>
-					Manage your Request in a simple way. Change Moving Dates, Travel Time and more.
-				</p>
-			</div>
-			<div><img src="/images/marketing.png" /></div>
-		</div>
-	</div>
-</div>
-
-<!--- 
-<div id="slider">
-	<div class="slider-container">
-		<div id="slides">
-			<div class="slide">
-				<img src="/images/calculator.png" alt="slide one">
-				<div class="caption">
-					<h2><i class="fas fa-calculator"></i> Moving Cost Calculator</h2>
-					<p>
-						A simple 2 steps Calculator for your clients to get a Moving Etimate<br>
-						And for you to get an information about the lead.</p>
-				</div>
-			</div>
-			<div class="slide">
-				<img src="/images/requests.png" alt="slide two">
-				<div class="caption">
-					<h2><i class="far fa-id-card"></i> Moving Requests</h2>
-					<p>
-						Manage your Request in a simple way. Change Moving Dates, Travel Time and more.
-					</p>
-				</div>
-			</div>
-			<div class="slide">
-				<img src="/images/schedule.png" alt="Moving CRM Schedule">
-				<div class="caption">
-					<h2><i class="far fa-calendar-alt"></i> Moving Schedule</h2>
-					<p>
-					Create Moving Schedule and manage all works through <b>Moving Calendar</b><br>
-						Everything you need in a simple design.
-					</p>
-				</div>
-			</div>
-			<div class="slide">
-				<img src="http://res.cloudinary.com/hurricane10/image/upload/v1499778117/img-23_pumtse.jpg" alt="slide two">
-				<div class="caption">
-					<h2><i class="fas fa-users"></i> Crew Management</h2>
-					<p>
-					Manage yout teams and assign them to works<br>
-							Provide information to your workers about your customer.
-					</p>
-				</div>
-			</div>
-			<div class="slide">
-				<img src="http://res.cloudinary.com/hurricane10/image/upload/v1499778117/img-23_pumtse.jpg" alt="slide two">
-				<div class="caption">
-					<h2><i class="fas fa-mail-bulk"></i> Marketing Autmation</h2>
-					<p>
-						Manage your Request in a simple way. Change Moving Dates, Travel Time and more.
-					</p>
-				</div>
-			</div>
-		</div>
-<span class="timeline"></span>
-
-</div>
-</div>
- --->
-<!-- 
-<div class="section justify-content-center">
-	<h2 class="title text-center">What is MovingCRM?</h2>
-	<br>
-	<ul class="nav nav-pills nav-pills-primary justify-content-center" role="tablist">
-	    <li class="nav-item col-md-2">
-	        <a class="nav-link active" data-toggle="tab" href="#link1" role="tablist" aria-expanded="true">
-						<i class="fas fa-calculator"></i>Calculator
-	        </a>
-	    </li>
-	    <li class="nav-item col-md-2">
-	        <a class="nav-link" data-toggle="tab" href="#link2" role="tablist" aria-expanded="false">
-	           <i class="far fa-id-card"></i>Requests
-	        </a>
-	    </li>
-	    <li class="nav-item col-md-2">
-	        <a class="nav-link" data-toggle="tab" href="#link3" role="tablist" aria-expanded="false">
-	           <i class="far fa-calendar-alt"></i>Schedule
-	        </a>
-	    </li>
+<div class="container">
+	<div class="section justify-content-center">
+		<h2 class="title text-center">What is MovingCRM?</h2>
+		<br>
+		<ul class="nav nav-pills nav-pills-primary justify-content-center" role="tablist">
 			<li class="nav-item col-md-2">
-	        <a class="nav-link" data-toggle="tab" href="#link4" role="tablist" aria-expanded="false">
-	           <i class="fas fa-users"></i>Management
-	        </a>
-	    </li>
+				<a class="nav-link active" data-toggle="tab" href="#link1" role="tablist" aria-expanded="true">
+							<i class="fas fa-calculator"></i>Calculator
+				</a>
+			</li>
 			<li class="nav-item col-md-2">
-	        <a class="nav-link" data-toggle="tab" href="#link5" role="tablist" aria-expanded="false">
-	           <i class="fas fa-mail-bulk"></i>Marketing
-	        </a>
-	    </li>
-	</ul>
-	<div class="tab-content tab-space">
-	    <div class="tab-pane fade in active" id="link1" aria-expanded="true">
-	      <div class="row">
-				<div class="col-md-4">
-					<div class="tabs-title">
-						Moving Calculator
-					</div>
-					<div class="tabs-text">
-						A simple 2 steps Calculator for your clients to get a Moving Etimate<br>
-							And for you to get an information about the lead.
-							<br>
-							<br>
-							<div class="text-center">
-								<a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
-							Free Online Demo
-						<div class="ripple-container"></div>
-							</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-8">
-					<img src="/images/calculator.png" class="rounded image-shadow"  width="100%"/>
-				</div>
-			</div>
-	    </div>
-	    <div class="tab-pane fade" id="link2" aria-expanded="false">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="tabs-title">
-							Moving Requests
-						</div>
-						<div class="tabs-text">
-							Manage your Request in a simple way. Change Moving Dates, Travel Time and more.
-							 <br>
-							 <br>
-							 <div class="text-center">
-								 <a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
-										 Free Online Demo
-									<div class="ripple-container"></div>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-8">
-						<img src="/images/requests.png" class="rounded image-shadow"  width="100%"/>
-					</div>
-				</div>
-	    </div>
-	    <div class="tab-pane fade" id="link3" aria-expanded="false">
+				<a class="nav-link" data-toggle="tab" href="#link2" role="tablist" aria-expanded="false">
+				<i class="far fa-id-card"></i>Requests
+				</a>
+			</li>
+			<li class="nav-item col-md-2">
+				<a class="nav-link" data-toggle="tab" href="#link3" role="tablist" aria-expanded="false">
+				<i class="far fa-calendar-alt"></i>Schedule
+				</a>
+			</li>
+				<li class="nav-item col-md-2">
+				<a class="nav-link" data-toggle="tab" href="#link4" role="tablist" aria-expanded="false">
+				<i class="fas fa-users"></i>Management
+				</a>
+			</li>
+				<li class="nav-item col-md-2">
+				<a class="nav-link" data-toggle="tab" href="#link5" role="tablist" aria-expanded="false">
+				<i class="fas fa-mail-bulk"></i>Marketing
+				</a>
+			</li>
+		</ul>
+		<div class="tab-content tab-space">
+			<div class="tab-pane fade in active" id="link1" aria-expanded="true">
 			<div class="row">
-				<div class="col-md-4">
-					<div class="tabs-title">
-						Moving Schedule
-					</div>
-					<div class="tabs-text">
-						Create Moving Schedule and manage all works through Moving Calendar<br>
-						Everything you need in a simple design.
-							<br>
-							<br>
-							<div class="text-center">
-								<a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
-										Free Online Demo
+						<div class="col-md-4">
+							<div class="tabs-title">
+								Moving Calculator
+							</div>
+							<div class="tabs-text">
+								A simple 2 steps Calculator for your clients to get a Moving Etimate<br>
+								And for you to get an information about the lead.
+								<br>
+								<br>
+								<div class="text-center">
+									<a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
+								Free Online Demo
 								<div class="ripple-container"></div>
-							</a>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<img src="/images/calculator.png" class="rounded image-shadow"  width="100%"/>
 						</div>
 					</div>
-				</div>
-				<div class="col-md-8">
-					<img src="/images/schedule.png" class="rounded image-shadow"  width="100%"/>
-				</div>
 			</div>
-	    </div>
-			<div class="tab-pane fade" id="link4" aria-expanded="false">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="tabs-title">
-							Team Management
-						</div>
-						<div class="tabs-text">
-							Manage yout teams and assign them to works<br>
-							 Provide information to your workers about your customer.
-							 <br>
-							 <br>
-							 <div class="text-center">
-								 <a href="#contact" class="btn btn-outline-primary  btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
-										 Free Online Demo
-									<div class="ripple-container"></div>
-								</a>
+			<div class="tab-pane fade" id="link2" aria-expanded="false">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="tabs-title">
+								Moving Requests
+							</div>
+							<div class="tabs-text">
+								Manage your Request in a simple way. Change Moving Dates, Travel Time and more.
+								<br>
+								<br>
+								<div class="text-center">
+									<a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
+											Free Online Demo
+										<div class="ripple-container"></div>
+									</a>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-md-8">
-						<img src="/images/team.png" class="rounded  image-shadow"  width="100%"/>
-					</div>
-				</div>
-	    </div>
-			<div class="tab-pane fade" id="link5" aria-expanded="false">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="tabs-title">
-							Marketing Automation
+						<div class="col-md-8">
+							<img src="/images/requests.png" class="rounded image-shadow"  width="100%"/>
 						</div>
-						<div class="tabs-text">
-							<br>
-							 Emails, SMS and more to automate your Moving Marketing Processes.
-							 <br>
-							 <br>
-							 <div class="text-center">
-								 <a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
-										 Free Online Demo
-									<div class="ripple-container"></div>
-								</a>
+					</div>
+			</div>
+			<div class="tab-pane fade" id="link3" aria-expanded="false">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="tabs-title">
+								Moving Schedule
+							</div>
+							<div class="tabs-text">
+								Create Moving Schedule and manage all works through Moving Calendar<br>
+								Everything you need in a simple design.
+								<br>
+								<br>
+								<div class="text-center">
+									<a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
+											Free Online Demo
+										<div class="ripple-container"></div>
+									</a>
+								</div>
 							</div>
 						</div>
+						<div class="col-md-8">
+							<img src="/images/schedule.png" class="rounded image-shadow"  width="100%"/>
+						</div>
 					</div>
-					<div class="col-md-8">
-						<img src="/images/marketing.png" class="rounded image-shadow"  width="100%"/>
+			</div>
+				<div class="tab-pane fade" id="link4" aria-expanded="false">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="tabs-title">
+								Team Management
+							</div>
+							<div class="tabs-text">
+								Manage yout teams and assign them to works<br>
+								Provide information to your workers about your customer.
+								<br>
+								<br>
+								<div class="text-center">
+									<a href="#contact" class="btn btn-outline-primary  btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
+											Free Online Demo
+										<div class="ripple-container"></div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<img src="/images/team.png" class="rounded  image-shadow"  width="100%"/>
+						</div>
 					</div>
-				</div>
-	    </div>
+			</div>
+				<div class="tab-pane fade" id="link5" aria-expanded="false">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="tabs-title">
+								Marketing Automation
+							</div>
+							<div class="tabs-text">
+								<br>
+								Emails, SMS and more to automate your Moving Marketing Processes.
+								<br>
+								<br>
+								<div class="text-center">
+									<a href="#contact" class="btn btn-outline-primary btn-round btn-lg text-muted" id="free-online-demo-t" style="margin-bottom:10px;">
+											Free Online Demo
+										<div class="ripple-container"></div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<img src="/images/marketing.png" class="rounded image-shadow"  width="100%"/>
+						</div>
+					</div>
+			</div>
+		</div>
 	</div>
-</div> -->
+</div>
