eab1d7f95b884b3b0ec48ecb11fa25919d202e3a
Taras Kim <kimtaras@gmail.com>
2020-06-25 08:54:19 -0400

    fixes

--- resources/views/sections/prices-table.blade.php
+++ resources/views/sections/prices-table.blade.php
@@ -43,7 +43,8 @@
             <ul>
 							<li><i class="material-icons text-primary">check</i> Moving Calculator</li>
               <li><i class="material-icons text-primary">check</i> Schedule</li>
-							<li><i class="material-icons text-primary">check</i> Dispatch</li>
+              <li><i class="material-icons text-primary">check</i> Dispatch</li>
+              <li><i class="material-icons text-primary">check</i> Inventory</li>
               <li><i class="material-icons text-primary">check</i> Emails</li>
 							<li><i class="material-icons text-primary">check</i> Secured Database</li>
 							<li><i class="material-icons text-primary">check</i> Personal Assistant</li>
@@ -68,7 +69,8 @@
             <ul>
 							<li><i class="material-icons text-primary">check</i> Moving Calculator</li>
               <li><i class="material-icons text-primary">check</i> Schedule</li>
-							<li><i class="material-icons text-primary">check</i> Dispatch</li>
+              <li><i class="material-icons text-primary">check</i> Dispatch</li>
+              <li><i class="material-icons text-primary">check</i> Inventory</li>
               <li><i class="material-icons text-primary">check</i> Emails</li>
 							<li><i class="material-icons text-primary">check</i> Secured Database</li>
 							<li><i class="material-icons text-primary">check</i> Personal Assistant</li>
@@ -93,7 +95,8 @@
             <ul>
 							<li><i class="material-icons text-primary">check</i> Moving Calculator</li>
               <li><i class="material-icons text-primary">check</i> Schedule</li>
-							<li><i class="material-icons text-primary">check</i> Team Management</li>
+              <li><i class="material-icons text-primary">check</i> Dispatch</li>
+              <li><i class="material-icons text-primary">check</i> Inventory</li>
               <li><i class="material-icons text-primary">check</i> Emails</li>
 							<li><i class="material-icons text-primary">check</i> Secured Database</li>
 							<li><i class="material-icons text-primary">check</i> Personal Assistant</li>
